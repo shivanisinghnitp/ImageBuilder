a3c56135cf453eff7e98a3419189aa885e0d50ce
ZubaeyrMSFT <zmohammed@microsoft.com>
2023-06-01 13:12:13 +0530

    disabling autorestart for prems supervisor processes

--- GenerateDockerFiles/wordpress/wordpress/local_bin/filesync.sh
+++ GenerateDockerFiles/wordpress/wordpress/local_bin/filesync.sh
@@ -30,7 +30,9 @@ do
 
 	# start unison for continuous filesync
 	if [ $(grep "INITIAL_FILESYNC_COMPLETED" $FILESYNC_STATUS_FILE_PATH) ] && [ ! $(grep "UNISON_PROCESS_STARTED" $FILESYNC_STATUS_FILE_PATH) ] \
-	&& supervisorctl start unison; then
+	&& supervisorctl start unison
+	&& supervisorctl start perms-service
+	&& supervisorctl start inotifywait-perms-service; then
 		echo "UNISON_PROCESS_STARTED" >> $FILESYNC_STATUS_FILE_PATH
 		break
 	fi
